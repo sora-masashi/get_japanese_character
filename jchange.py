d28520369dfa15726d16e58ca7404270646f7d9f
sora-masashi <33551388+sora-masashi@users.noreply.github.com>
2017-11-15 23:47:27 +0900

    Add files via upload
    second_version bag///

--- jchange.py
+++ jchange.py
@@ -19,7 +19,6 @@ def get_japanese_date(given_date):
         m = re.match(japanese,line)
         if m:
             dict_date.append(line)
-            print(line)
 
     first_file = codecs.open("japanese.json","w","utf-8")
     json.dump(dict_date,first_file,indent=2,sort_keys=True,ensure_ascii=False)
@@ -29,19 +28,46 @@ def get_japanese_date(given_date):
     print("Your date was changed into 'Japanese only'! Successflly saved as 'japanese.json'!")
 
 
-def change_hiragana(given_date,x,goo_api_id):
+def change_hiragana(given_date,x,y,goo_api_id):
     """given_dateの1つ目からxつ目までをひらがなに変換し、リスト化、"hiragana.json"に保存する。"""
     print("Now working...")
     japanese_json = open(given_date,"r")
     jsondate = json.load(japanese_json)
-    jsondate = [x for x in jsondate[1:x+1]]
+    jsondate = [x for x in jsondate[x:y+1]]
     sending_date = ",".join(jsondate)
     params = {"app_id":goo_api_id,"sentence":sending_date, "output_type":"hiragana"}
     responses = requests.post(
     "https://labs.goo.ne.jp/api/hiragana",
     params)
-    print(responses.content)
+    old_get_date = responses.content.split(",")
+    get_date = []
+    for line in old_get_date:
+        line = line.strip()
+        line = unicode(line)
+        get_date.append(line)
+    save_file = open("hiragana.json","rt")
+    save = json.load(save_file)
+    previous = []
+
+    for i in save:
+        previous.append(i)
+
+    get_date += previous
     save_file = open("hiragana.json","wt")
-    json.dump(responses.content,save_file)
+    json.dump(get_date,save_file)
     save_file.close()
     print("Your date was changed into 'hiragana only'.Successflly saved as 'hiragana.json'")
+
+def find(firststring,given_list):
+    """given_listの中からfirrststringで始まり、finalstringで終わる文字をリストとして返す"""
+    get_list = []
+    for line in given_list:
+        c = re.compile(firststring)
+        m = re.match(c,line)
+        print(line)
+        if m:
+            get_list.append(line)
+    return get_list
+
+get_japanese_date("jawiki-latest-all-titles-in-ns0")
+change_hiragana("japanese.json",1,3000,"212ddcfeb4b83a547a2fff60b117d2735babf01080854f1197bd7bfea6fb5a2d")
